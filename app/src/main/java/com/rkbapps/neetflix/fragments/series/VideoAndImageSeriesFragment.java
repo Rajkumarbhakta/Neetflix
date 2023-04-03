2cece827ee2d6205609ac0ecc6063b5174f9c38e
Rajkumar Bhakta <email.rajkumarbhakta@gmail.com>
2023-04-04 01:12:53 +0530

    series ui refinement

--- app/src/main/java/com/rkbapps/neetflix/fragments/series/VideoAndImageSeriesFragment.java
+++ app/src/main/java/com/rkbapps/neetflix/fragments/series/VideoAndImageSeriesFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -13,7 +15,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.rkbapps.neetflix.R;
 import com.rkbapps.neetflix.adapter.BackdropAdapter;
 import com.rkbapps.neetflix.adapter.PosterAdapter;
+import com.rkbapps.neetflix.adapter.VideoAdapter;
 import com.rkbapps.neetflix.models.images.ImagesModel;
+import com.rkbapps.neetflix.models.videos.VideoModel;
 import com.rkbapps.neetflix.services.ApiData;
 import com.rkbapps.neetflix.services.RetrofitInstance;
 import com.rkbapps.neetflix.services.TvSeriesApi;
@@ -26,6 +30,7 @@ import retrofit2.Response;
 public class VideoAndImageSeriesFragment extends Fragment {
     TvSeriesApi tvSeriesApi = RetrofitInstance.getTvSeriesApi();
     private RecyclerView backdrops, videos, posters;
+    private TextView txtBackdrop,txtPoster,txtVideos;
 
     public VideoAndImageSeriesFragment() {
         // Required empty public constructor
@@ -43,10 +48,21 @@ public class VideoAndImageSeriesFragment extends Fragment {
         videos = view.findViewById(R.id.recyclerSeriesVideos);
         posters = view.findViewById(R.id.recyclerSeriesPosters);
 
+        txtBackdrop = view.findViewById(R.id.txtBackdropSeries);
+        txtVideos = view.findViewById(R.id.txtVideosSeries);
+        txtPoster = view.findViewById(R.id.txtPosterSeries);
+
+
+        txtBackdrop.setVisibility(View.GONE);
+        txtVideos.setVisibility(View.GONE);
+        txtPoster.setVisibility(View.GONE);
+
         backdrops.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
         posters.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
-        loadSeriesImages(id);
+        videos.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
 
+        loadSeriesImages(id);
+        loadSeriesVideos(id);
 
         return view;
     }
@@ -57,13 +73,58 @@ public class VideoAndImageSeriesFragment extends Fragment {
             @Override
             public void onResponse(Call<ImagesModel> call, Response<ImagesModel> response) {
                 if (response.isSuccessful()) {
-                    posters.setAdapter(new PosterAdapter(getContext(), response.body().getPosters()));
-                    backdrops.setAdapter(new BackdropAdapter(getContext(), response.body().getBackdrops()));
+                    if(response.body()!=null){
+                        if(response.body()!=null){
+                            if(response.body().getPosters().size()!=0 && response.body().getPosters()!=null){
+                                txtPoster.setVisibility(View.VISIBLE);
+                                posters.setAdapter(new PosterAdapter(getContext(), response.body().getPosters()));
+                            }else{
+                                posters.setVisibility(View.GONE);
+                                txtPoster.setVisibility(View.GONE);
+                            }
+
+                            if(response.body().getBackdrops().size()!=0 && response.body().getBackdrops()!=null){
+                                txtBackdrop.setVisibility(View.VISIBLE);
+                                backdrops.setAdapter(new BackdropAdapter(getContext(), response.body().getBackdrops()));
+                            }else {
+                                backdrops.setVisibility(View.GONE);
+                                txtBackdrop.setVisibility(View.GONE);
+                            }
+                        }
+                    }else {
+                        Toast.makeText(getContext(), response.message(), Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
 
             @Override
             public void onFailure(Call<ImagesModel> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void loadSeriesVideos(int id) {
+        tvSeriesApi.getSeriesVideos(id, ApiData.API_KEY).enqueue(new Callback<VideoModel>() {
+            @Override
+            public void onResponse(Call<VideoModel> call, Response<VideoModel> response) {
+                if (response.isSuccessful()) {
+                    if (response.body() != null) {
+                        if (response.body().getResults() != null && response.body().getResults().size() != 0) {
+                            txtVideos.setVisibility(View.VISIBLE);
+                            videos.setAdapter(new VideoAdapter(getContext(), response.body().getResults()));
+                        }else {
+                            videos.setVisibility(View.GONE);
+                            txtVideos.setVisibility(View.GONE);
+                        }
+                    }
+                }else {
+                    Toast.makeText(getContext(), response.message(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<VideoModel> call, Throwable t) {
 
             }
         });
